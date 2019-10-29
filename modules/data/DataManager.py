007338fab94a8d4abd3b5a964127f561bfc80519
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-29 05:44:55 +0000

    Masked image data generator.

--- modules/data/DataManager.py
+++ modules/data/DataManager.py
@@ -68,12 +68,16 @@ class DataManager:
                 preprocessing_function=preprocessing_function,
                 validation_split=self.config["validation_split"],
             )
+            datagen2 = ImageDataGenerator(
+#                 preprocessing_function=preprocessing_function,
+                validation_split=self.config["validation_split"],
+            )
             
-            directory = f"data/kenya/{self.config['image_size']}/{self.config['resizing']}"
+            directory = f"{modules.data.util.root()}/kenya/{self.config['image_size']}/{self.config['resizing']}"
             
             if not os.path.exists(directory):
                 print(f"Directory {directory} does not exist. Falling back to concurrent resizing.")
-                directory = f"data/kenya/kenya_1000x1000_images"
+                directory = f"{modules.data.util.root()}/kenya/kenya_1000x1000_images"
             
             dataframe = pd.DataFrame(
                 list(map(
@@ -112,12 +116,59 @@ class DataManager:
                     shuffle=self.config["shuffle"],
                     target_size=(self.config["image_size"], self.config["image_size"])
                 )
-            
-            return train_generator, val_generator, dataframe
-            
+            elif self.config["mask"] == 'overlay':
+                raise NotImplementedError()
+            elif self.config["mask"] == 'occlude':
+                mask_df = pd.DataFrame(list(map(
+                    lambda e: (f"{e[0]}_kenya_224x224_mask_10.png", e[2]), 
+                    zip(
+                        self.dataframes['kenya'].index, 
+                        map(int, self.dataframes['kenya']["id"]),
+                        self.dataframes['kenya']["class"]
+                    )
+                    )),
+                columns=["filename", "class"])
+
+                mask_df = mask_df.iloc[dataframe.index]
+                mask_directory = f"{modules.data.util.root()}/kenya/kenya_224x224_masks_10/"
+                train_generator=self.multiple_generator(datagen, datagen2, dataframe, mask_df, directory, mask_directory, 'training')
+                val_generator=self.multiple_generator(datagen, datagen2, dataframe, mask_df, directory, mask_directory, 'validation')             
         else:
             raise NotImplementedError("Custom model and preprocessing pipeline not yet defined.")
+        return train_generator, val_generator
 
 
     def generate_peru(self):
         raise NotImplementedError()
+        
+        
+    def multiple_generator(self, generator, generator_no_preprocess, img_df, mask_df, img_dir, mask_dir, subset):
+        img_generator = generator.flow_from_dataframe(
+            img_df,
+            directory=img_dir, 
+            subset=subset,
+            class_mode='categorical',
+            batch_size=self.config["batch_size"],
+            seed=self.config["seed"],
+            shuffle=self.config["shuffle"],
+            target_size=(self.config["image_size"], self.config["image_size"])
+        )
+        mask_generator = generator_no_preprocess.flow_from_dataframe(
+            mask_df,
+            directory=mask_dir, 
+            subset=subset,
+            class_mode='categorical',
+            batch_size=self.config["batch_size"],
+            seed=self.config["seed"],
+            shuffle=self.config["shuffle"],
+            target_size=(self.config["image_size"], self.config["image_size"])
+        )
+        while True:
+            img, label = img_generator.next()
+            mask, _ = mask_generator.next()
+            print(img)
+            print(mask)
+            print(np.sum(mask))
+            print(np.max(mask))
+            # np.flip to account for upside-down mask
+            yield img.astype(np.float32), (img * np.flip(mask, axis=1)).astype(np.float32), label
