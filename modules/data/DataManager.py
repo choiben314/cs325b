41a2b2aefa17137281d548477077e263c8c4f348
jkamalu <john.kamalu@gmail.com>
2019-11-01 20:17:54 -0700

    Cleaning and fixing DataManager class

--- modules/data/DataManager.py
+++ modules/data/DataManager.py
@@ -41,30 +41,19 @@ class DataManager:
             classes = [self.config["class_enum"][v] for v in self.dataframes[country]["class"].values]
             self.dataframes[country]["label"] = classes
             
-            # indices only needed if need to 
-            # self.indices[country] = set(modules.data.util.load_image_indices(country))
-            
-            self.dataframes["kenya"]["valid"] = self.file_is_valid(self.dataframes["kenya"], "data/kenya/224/cropped")
+            directory = f"{modules.data.util.root()}/{country}/{self.config['image_size']}/{self.config['resizing']}"
+            valid = self.file_is_valid(self.dataframes["kenya"], directory)
+            self.dataframes[country] = self.dataframes[country][valid]
 
             self._setup_countries.add(country)
 
-    def extract_filenames(self, directory):
-        filenames = map(lambda x: x.strip(), os.listdir(directory))
-        filenames = set(filenames)
-        return filenames
-
     def file_is_valid(self, dataframe, directory):
-        filenames = self.extract_filenames(directory)
-
+        filenames = self._extract_filenames(directory)
         valid = []
         for i in dataframe.index:
-            fname = f"{i}_{int(dataframe['id'].loc[i])}.jpg"
+            fname = self._format_filename(i, int(dataframe['id'].loc[i]))
             valid.append(fname in filenames)
-
-        del filenames
-
-        return valid
-            
+        return np.array(valid)
 
     def class_weight(self, country):
         class_weight = None
@@ -78,117 +67,134 @@ class DataManager:
 
     
     def generate_kenya(self):
+        
+        # get input directory
+        directory = f"{modules.data.util.root()}/kenya/{self.config['image_size']}/{self.config['resizing']}"
+        
+        # format dataframe for ImageDataGenerator.flow_from_dataframe
+        dataframe = self._format_dataframe_for_flow("kenya")
+        
+        # sample the data
+        if self.config["sample"]:
+            dataframe = dataframe.sample(n=self.config["sample"]["size"], replace=False, random_state=self.config["seed"])
+
+        # define data preprocessing
         preprocessing_function = None
         if self.config["pretrained"]:
             module = modules.models.pretrained_cnn_module(self.config["pretrained"]["type"])
             preprocessing_function = getattr(module, "preprocess_input")
-            
-            datagen = ImageDataGenerator(
-                preprocessing_function=preprocessing_function,
-                validation_split=self.config["validation_split"],
-            )
-            
-            directory = f"{modules.data.util.root()}/kenya/{self.config['image_size']}/{self.config['resizing']}"
-            
-            if not os.path.exists(directory):
-                print(f"Directory {directory} does not exist. Falling back to concurrent resizing.")
-                directory = f"{modules.data.util.root()}/kenya/kenya_1000x1000_images"
-            
-            dataframe = pd.DataFrame(
-                list(map(
-                    lambda e: (f"{e[0]}_{e[1]}.jpg", e[2], e[3]), 
-                    zip(
-                        self.dataframes['kenya'].index, 
-                        map(int, self.dataframes['kenya']["id"]),
-                        self.dataframes['kenya']["class"],
-                        self.dataframes['kenya']['valid']
-                    )
-                )),
-                columns=["filename", "class", "valid"]
-            )
-            
-            if self.config["sample"]:
-                dataframe = dataframe[dataframe["valid"] == True].sample(n=self.config["sample"]["size"], replace=False, random_state=self.config["seed"])[["filename", "class"]]
-                
-            if self.config["mask"] == 'none':
-                train_generator = datagen.flow_from_dataframe(
-                    dataframe,
-                    directory=directory, 
-                    subset="training",
-                    class_mode='categorical',
-                    batch_size=self.config["batch_size"],
-                    seed=self.config["seed"],
-                    shuffle=self.config["shuffle"],
-                    target_size=(self.config["image_size"], self.config["image_size"])
-                )
+        else:
+            raise NotImplementedError("Custom model and preprocessing pipeline not yet defined.")
+        
+        # make image data generator for rgb
+        datagen = ImageDataGenerator(
+            preprocessing_function=preprocessing_function,
+            validation_split=self.config["validation_split"],
+        )
+        
+        if self.config["mask"] == 'none':
+            train_generator = self._build_generator(datagen, dataframe, directory, "training")
+            val_generator = self._build_generator(datagen, dataframe, directory, "validation")            
+        elif self.config["mask"] == "occlude":
+            datagen_mask = ImageDataGenerator(validation_split=self.config["validation_split"])
 
-                val_generator = datagen.flow_from_dataframe(
-                    dataframe,
-                    directory=directory, 
-                    subset="validation",
-                    class_mode='categorical',
-                    batch_size=self.config["batch_size"],
-                    seed=self.config["seed"],
-                    shuffle=self.config["shuffle"],
-                    target_size=(self.config["image_size"], self.config["image_size"])
-                )
-            elif self.config["mask"] == 'overlay':
-                raise NotImplementedError()
-                datagen_mask = ImageDataGenerator(
-                    validation_split=self.config["validation_split"],
-                )
-            elif self.config["mask"] == 'occlude':
-                datagen_mask = ImageDataGenerator(
-                    validation_split=self.config["validation_split"],
-                )
-                
-                mask_df = pd.DataFrame(list(map(
+            ##
+            #
+            # TODO: replace with more general DataManager_format_dataframe_for_flow
+            #
+            ##
+            dataframe_mask = pd.DataFrame(
+                list(map(
                     lambda e: (f"{e[0]}_kenya_224x224_mask_10.png", e[2]), 
                     zip(
-                        self.dataframes['kenya'].index, 
+                        self.dataframes['kenya'].index,
                         map(int, self.dataframes['kenya']["id"]),
                         self.dataframes['kenya']["class"]
                     )
-                    )),
+                )),
                 columns=["filename", "class"])
+            dataframe_mask = dataframe_mask.iloc[dataframe.index]
+            directory_mask = f"{modules.data.util.root()}/kenya/kenya_224x224_masks_10/"
 
-                mask_df = mask_df.iloc[dataframe.index]
-                mask_directory = f"{modules.data.util.root()}/kenya/kenya_224x224_masks_10/"
+            train_generator = self.multiple_generator(
+                datagen, datagen_mask, 
+                dataframe, dataframe_mask, 
+                directory, directory_mask, 
+                'training'
+            )
+            val_generator = self.multiple_generator(
+                datagen, datagen_mask, 
+                dataframe, dataframe_mask, 
+                directory, directory_mask, 
+                'validation'
+            )
                 
-                train_generator=self.multiple_generator(datagen, datagen_mask, dataframe, mask_df, directory, mask_directory, 'training')
-                val_generator=self.multiple_generator(datagen, datagen_mask, dataframe, mask_df, directory, mask_directory, 'validation')             
-        else:
-            raise NotImplementedError("Custom model and preprocessing pipeline not yet defined.")
+        elif self.config["mask"] == "overlay":
+            ##
+            #
+            # TODO: build out overlay (4th channel) logic
+            #
+            ##
+            raise NotImplementedError("4th channel overlay not implemented yet.")
+
         return train_generator, val_generator
 
 
     def generate_peru(self):
+        ##
+        #
+        # TODO: build out peru logic
+        #
+        ##
         raise NotImplementedError()
         
         
-    def multiple_generator(self, generator, generator_no_preprocess, img_df, mask_df, img_dir, mask_dir, subset):
-        img_generator = generator.flow_from_dataframe(
-            img_df,
-            directory=img_dir, 
-            subset=subset,
-            class_mode='categorical',
-            batch_size=self.config["batch_size"],
-            seed=self.config["seed"],
-            shuffle=self.config["shuffle"],
-            target_size=(self.config["image_size"], self.config["image_size"])
-        )
-        mask_generator = generator_no_preprocess.flow_from_dataframe(
-            mask_df,
-            directory=mask_dir, 
-            subset=subset,
-            class_mode='categorical',
-            batch_size=self.config["batch_size"],
-            seed=self.config["seed"],
-            shuffle=self.config["shuffle"],
-            target_size=(self.config["image_size"], self.config["image_size"])
-        )
+    def multiple_generator(self, datagen1, datagen2, dataframe1, dataframe2, directory1, directory2, subset):
+        generator1 = self._build_generator(datagen1, dataframe1, directory1, subset)
+        generator2 = self._build_generator(datagen2, dataframe2, directory2, subset)
         while True:
-            img, label = img_generator.next()
-            mask, _ = mask_generator.next()
-            # np.flip to account for upside-down mask
-            yield (img * np.flip(mask, axis=1)).astype(np.float32), label
+            x1, y1 = generator1.next()
+            x2, y2 = generator2.next()
+            ##
+            #
+            # TODO: remove np.flip for upside-down masks
+            #
+            ##
+            yield (x1 * np.flip(x2, axis=1)).astype(np.float32), y1
+            
+    def _build_generator(self, datagen, dataframe, directory, subset):
+        return datagen.flow_from_dataframe(
+            dataframe,
+            directory=directory, 
+            subset=subset,
+            class_mode='categorical',
+            batch_size=self.config["batch_size"],
+            seed=self.config["seed"],
+            shuffle=self.config["shuffle"],
+            target_size=(self.config["image_size"], self.config["image_size"])
+        )
+            
+    def _format_dataframe_for_flow(self, country, suffix=None):
+        return pd.DataFrame(
+            list(map(
+                lambda e: (self._format_filename(e[0], e[1], suffix=suffix), e[2]), 
+                zip(
+                    self.dataframes[country].index, 
+                    map(int, self.dataframes[country]["id"]),
+                    self.dataframes[country]["class"]
+                )
+            )),
+            columns=["filename", "class"]
+        )
+            
+    def _format_filename(self, id1, id2, suffix=None, ext="jpg"):
+        if suffix is None:
+            return f"{id1}_{id2}.{ext}"
+        else:
+            return f"{id1}_{id2}_{suffix}.{ext}"
+    
+    def _extract_filenames(self, directory):
+        filenames = map(lambda x: x.strip(), os.listdir(directory))
+        filenames = set(filenames)
+        return filenames
+
